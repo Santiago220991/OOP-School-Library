22397ad9483100ae0d3fdbb730a00e6dca196341
Santiago <alexander.cardenas10@outlook.com>
2022-07-19 18:30:59 -0500

    Modify case statement

--- main.rb
+++ main.rb
@@ -1,25 +1,25 @@
+require './app'
 def main
+    app=App.new()
+    exit=0
+    while(exit==0)
+    print("Welcome to School Library App! 
 
-exit=0
-while(exit==0)
-print("Welcome to School Library App! 
+        Please choose an option by entering a number:
+        1 - List all books
+        2 - List all people
+        3 - Create a person
+        4 - Create a book
+        5 - Create a rental 
+        6 - List all rentals for a given person id
+        7 - Exit
+            ")
+        option=gets.chomp.to_i
 
-Please choose an option by entering a number:
-1 - List all books
-2 - List all people
-3 - Create a person
-4 - Create a book
-5 - Create a rental 
-6 - List all rentals for a given person id
-7 - Exit
-    ")
-option=gets.chomp.to_i
-
-if option==7
-    puts "Thanks for using the app"
-    exit=1
-end
-
-end
+        if option==7
+            exit=1
+        end
+        app.select_option(option)
+    end
 end
 main()
